9bf1529ef33ad69ef42657df739fdf27cbc5f3f5
KarthikRdec02 <kingskarthik67@gmail.com>
2023-02-07 13:47:12 +0530

    code added

--- src/Array.java
+++ src/Array.java
@@ -35,5 +35,6 @@ public static void main(String[] args) {
 	System.out.println(b[4]);
 	System.out.println("coded new");
 	System.out.println("just cheking");
+	System.out.println("good morning");
 }
 }
